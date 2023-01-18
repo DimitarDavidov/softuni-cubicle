14241514de1346b3fc375839de5de58e4fa1645b
DimitarDavidov <dimitardavidov5@gmail.com>
2023-01-18 19:23:41 +0200

    added engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const config = require('./config')
+
+
+const config = require('./config/config');
+const setupViewEngine = require('./config/viewEngine');
+
 const app = express();
+setupViewEngine(app);
 
 app.get('/', (req, res) => {
-    res.send('Home Page');
+    res.render('home');
 });
 
 app.listen(config.PORT, () => console.log(`Serve is running on port ${config.PORT}...`))
