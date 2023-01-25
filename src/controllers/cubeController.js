c2199ee0c289303b87ce025ba256e8f58eb2d5b3
DimitarDavidov <dimitardavidov5@gmail.com>
2023-01-26 01:02:58 +0200

    add details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,4 +1,5 @@
-const Cube = require('../models/Cube')
+const Cube = require('../models/Cube');
+const db = require('../db.json');
 
 exports.getNewCube = (req, res) => {
     res.render('create')
@@ -11,4 +12,20 @@ exports.postCreateCube = (req, res) => {
     Cube.save(cube);
     
     res.redirect('/')
+}
+
+exports.getDetails = (req, res) => {
+    let cubeId = +req.params.cubeId;
+
+    if(!cubeId){
+        return res.redirect('404');
+    }
+
+    let cube = db.cubes.find(x => x.id === cubeId);
+
+    if(!cube){
+        return res.redirect('404');
+    };
+
+    res.render('details', {cube});
 }
--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,6 @@ router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage)
 router.get('/create', cubeController.getNewCube);
 router.post('/create', cubeController.postCreateCube);
+router.get('/details/:cubeId', cubeController.getDetails);
 
 module.exports = router;
