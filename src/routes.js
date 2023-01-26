28f09ab9103c74270f0e2d2780d917646a960ccc
DimitarDavidov <dimitardavidov5@gmail.com>
2023-01-26 20:39:22 +0200

    accesssoryCreate page works

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
+const accessoryController = require('./controllers/accessoryController')
 
 const router = require('express').Router();
 
@@ -10,6 +11,8 @@ router.get('/404', homeController.getErrorPage);
 router.get('/create', cubeController.getNewCube);
 router.post('/create', cubeController.postCreateCube);
 router.get('/details/:cubeId', cubeController.getDetails);
+
+router.use('/accessory', accessoryController)
 
 
 module.exports = router;
