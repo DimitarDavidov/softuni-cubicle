9c9ba7cf85b6daa2224e00382c79f5ca17c1fad0
DimitarDavidov <dimitardavidov5@gmail.com>
2023-02-06 20:25:09 +0200

    check user and pass on login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const authService = require('../services/authService');
+
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
@@ -8,14 +10,37 @@ router.get('/register', (req,res) => {
     res.render('auth/register')
 });
 
-router.post('/register', (req, res) => {
+router.post('/login', async (req, res) => {
+    const {username, password } = req.body;
+
+    try{
+        await authService.login(username, password);
+    }catch(err){
+        console.log(err);
+        return res.redirect('/')
+    }
+    res.redirect('/')
+})
+
+router.post('/register', async (req, res) => {
     const {username, password, repeatPassword} = req.body;
     
     if(password !== repeatPassword){
-        return res.status(404).end();
+        return res.redirect('/404')
     };
 
-    
-})
+    const existingUser = await authService.getUserByUsername(username);
+
+    if(existingUser){
+        return res.redirect('/404')
+    }
+
+    const user = await authService.register(username, password);
+
+
+    res.redirect('/login')
+});
+
+
 
 module.exports = router
